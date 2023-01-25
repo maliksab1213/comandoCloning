1cb625149de3a887a2947c047ddd74d58eafa163
maliksab1213 <115742133+maliksab1213@users.noreply.github.com>
2023-01-25 22:13:26 +0500

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,11 @@
+import os
+os.system('xdg-open https://facebook.com/100053134242697/')
+#Comando kings
+#32bit/64bit 
+try:
+ import TestV1
+except:
+ try:
+  import TestV2
+ except:
+  print('Comando Kings Always Non-Stop')
